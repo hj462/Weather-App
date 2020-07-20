b00cc37b170a6e1ceb29074785fc574c7866f4d8
Haris Jamil <haris.jamil@synavos.cm>
2020-07-20 14:14:04 +0500

    Added new forecast API

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,22 @@
-const request = require('request')
+const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&
+    exclude=current&appid=defa7c6d3f09cf545dbc04d33731ac5e&units=metric`;
 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
-        }
-    })
-}
+  request({ url, json: true }, (error, { body }) => {
+    console.log(body);
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        " It is currently " + body.current.temp + " degress out."
+      );
+    }
+  });
+};
 
-module.exports = forecast
+module.exports = forecast;
